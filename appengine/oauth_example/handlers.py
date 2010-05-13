4164f4a290c68c8a9bd76f639e096575b35241cf
Bill Ferrell <wferell@gmail.com>
2010-05-12 22:43:23 -0700

    Adding to the app engine oauth example to show how to actually authentic a user and post to twitter.

--- appengine/oauth_example/handlers.py
+++ appengine/oauth_example/handlers.py
@@ -1,3 +1,4 @@
+import cgi
 import pickle
 from google.appengine.ext.webapp import RequestHandler, template
 from google.appengine.ext import db
@@ -46,7 +47,8 @@ class CallbackPage(RequestHandler):
             return
 
         # Lookup the request token
-        request_token = OAuthToken.gql("WHERE token_key=:key", key=oauth_token).get()
+        request_token = OAuthToken.gql("WHERE token_key=:key", 
+									   key=oauth_token).get()
         if request_token is None:
             # We do not seem to have this request token, show an error.
             print template.render('error.html', {'message': 'Invalid token!'})
@@ -67,6 +69,30 @@ class CallbackPage(RequestHandler):
         # So now we could use this auth handler.
         # Here we will just display the access token key&secret
         print template.render('oauth_example/callback.html', {
-            'access_token': auth.access_token
+            'access_token': auth.access_token,
         })
 
+class PostTweet(RequestHandler):
+ def post(self):
+    tweettext = str(cgi.escape(self.request.get('tweettext')))
+    # Normally the key and secret would not be passed but rather 
+    # stored in a DB and fetched for a user.
+    token_key = str(self.request.get('key'))
+    token_secret = str(self.request.get('secret'))
+
+    #Here we authenticate this app's credentials via OAuth
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+    #Here we set the credentials that we just verified and passed in.
+    auth.set_access_token(token_key, token_secret)
+
+    #Here we authorize with the Twitter API via OAuth
+    twitterapi = tweepy.API(auth)
+
+    #Here we update the user's twitter timeline with the tweeted text.
+    twitterapi.update_status(tweettext)
+
+    #Now we fetch the user information and redirect the user to their twitter
+    # username page so that they can see their tweet worked.
+    user = twitterapi.me()
+    self.redirect('http://www.twitter.com/%s' % user.screen_name)
